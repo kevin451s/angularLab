55ae3041b45f24df069c566d34ae70613a37a90f
Kevin Salazar <kskevin170@yahoo.com>
2016-11-30 11:26:54 -0800

    adds controller nad app.js module

---
+++ js/todoController.js
@@ -0,0 +1,22 @@
+(function() {
+  'use strict';
+  angular.module('angularTodo')
+    .controller('TodoController', TodoController)
+
+  function TodoController() {
+    var self = this;
+    self.add = addTodo
+    self.newTodo = {date: '', task: '', done: false}
+
+    self.all = [
+      {date: '12/30/16', task: 'Get a job', done: false},
+      {date: '1/30/17', task: 'Take a vacation.', done: false},
+      {date: '11/30/16', task: 'Learn Angular', done: true}
+    ]
+
+    function addTodo() {
+      self.all.push({date: self.newTodo.date, task: self.newTodo.task, done: false})
+      self.newTodo = {date: '',task: '', done: ''}
+    }
+  }
+}());
