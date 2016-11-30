28cbc1c45729202e7e29e911205a8713ecc32b57
Gaye Lowenstein <glowen@hotmail.com>
2016-11-30 12:11:43 -0800

    refactored to $scope

--- js/todoController.js
+++ js/todoController.js
@@ -3,20 +3,19 @@
   angular.module('angularTodo')
     .controller('TodoController', TodoController)
 
-  function TodoController() {
-    var self = this
-    self.add = addTodo
-    self.newTodo = {date: '', task: '', done: false}
+  function TodoController($scope) {
+    $scope.add = addTodo
+    $scope.newTodo = {date: '', task: '', done: false}
 
-    self.all = [
+    $scope.all = [
       {date: '12/30/16', task: 'Get a job', done: false},
       {date: '1/30/17', task: 'Take a vacation.', done: false},
       {date: '11/30/16', task: 'Learn Angular', done: true}
     ]
 
     function addTodo() {
-      self.all.push({date: self.newTodo.date, task: self.newTodo.task, done: false})
-      self.newTodo = {date: '',task: '', done: ''}
+      $scope.all.push({date: $scope.newTodo.date, task: $scope.newTodo.task, done: false})
+      $scope.newTodo = {date: '',task: '', done: ''}
     }
   }
 }());
